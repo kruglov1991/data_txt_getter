63c15553799db7dbd3a498549a466b3c20af1c76
kruglov1991 <144706153+kruglov1991@users.noreply.github.com>
2023-09-23 11:39:19 +0300

    fix: 5 blocks in a row

--- src/App.js
+++ src/App.js
@@ -294,7 +294,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
+    <div style={{maxWidth: '1730px'}} className="container mx-auto p-4">
       <h1 className="text-3xl font-bold text-center mb-8">Статистика</h1>
 
       {!isLoggedIn ? (
